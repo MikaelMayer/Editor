ebdb63704b8455bb3b073960656335bc3fcfdaf0
Mikaël Mayer <mikael.mayer@epfl.ch>
2019-01-04 15:32:52 -0600

    Basic authentication test

---
+++ test/authentication.elm
@@ -0,0 +1,72 @@
+<html>
+<head>
+<meta name="google-signin-client_id" content="844835838734-2iphm3ff20ephn906md1ru8vbkpu4mg8.apps.googleusercontent.com">
+<title>Hello authenticated world</title>
+<style>
+a.closeSignIn {
+  margin-left: 2px;
+  padding-left: 4px;
+  padding-right: 5px;
+}
+a.closeSignIn:hover {
+  background: #AAA;
+  color: white;
+  border-radius: 10px;
+}
+</style>
+</head>
+<body>
+<script>
+(setGhostOnInserted || []).push(insertedNode => insertedNode.tagName == "STYLE" && insertedNode.attributes.length == 0);
+(setGhostOnInserted || []).push(insertedNode =>
+  (insertedNode.tagName == "DIV" &&
+    insertedNode.classList.contains("abcRioButton")) ||
+  (insertedNode.tagName == "IFRAME" &&
+    insertedNode.getAttribute("id") == "ssIFrame_google")
+);
+</script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<h1>
+<img src=@(listDict.get "picture" user |> Maybe.withDefault "")> Hello @(listDict.get "given_name" user |> Maybe.withDefault "Anonymous")!</h1>
+<div class="g-signin2" data-onsuccess="onSignIn" list-ghost-attributes="data-gapiscan data-onload" children-are-ghost="true"></div>
+<script>
+addSignout = (name) => {
+  var signin = document.querySelector(".abcRioButtonContents").children[1];
+  signin.setAttribute("title", "Signed in as " + name);
+  var signout = document.createElement("a");
+  signout.classList.add("closeSignIn");
+  signout.setAttribute("title", "Sign out");
+  signout.innerText = "x";
+  signout.onclick = () => {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+      auth2.disconnect();
+      googleAuthIdToken = undefined;
+      console.log('User signed out.');
+    });
+  }
+  signin.append(signout);
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  
+  var wasSignedIn = googleAuthIdToken ? "true" : "false";
+  // When set, will be used throughout 
+  googleAuthIdToken = googleUser.getAuthResponse().id_token;
+  
+  /*console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  */
+  addSignout(profile.getName());
+  
+  if(!wasSignedIn) { // Necessary to ensure that we don't reload the page the second time it is loaded.
+    reloadPage();
+  }
+}
+</script>
+
+</body>
+</html>
