1fc849cf0032478b078d3faf5e14000c6a9c1e2a
Mikaël Mayer <mikael.mayer@epfl.ch>
2019-04-23 23:17:57 -0500

    Updated libraries and binaries

--- test/php/test.php
+++ test/php/test.php
@@ -1,15 +1,9 @@
 <?php
-
 $who = $_GET["who"];
 $what = "Hello $who";
-
 ?>
-<html>
-<head>
-</head>
-<body>
+<html><head></head><body>
 <div>
   <span><?php echo $what; ?>!</span>
 </div>
-</body>
-</html>
+</body></html>
